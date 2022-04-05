42681f013805cf26de178123a05b8825c01b20e1
Kenta Nishimoto <0tb1161j@gmail.com>
2022-04-05 15:56:16 +0900

    カートの準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_001848) do
+ActiveRecord::Schema.define(version: 2022_04_05_065430) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2022_04_05_001848) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 1, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
